d296a94f859e332c55cb90d9f7fa5e901b993f6b
Sébastien Strebelle <sebastien.strebelle@student.uclouvain.be>
2016-03-28 15:34:35 +0200

    main

--- main.java
+++ main.java
@@ -7,7 +7,14 @@ public class main {
 		System.out.println("test");
 	}
 
+
+
 	public static float[] pagerank(int[][] adj, float alpha, float[] pers) {
+		//G = aplha*P + (1-aplha) e*pers^T
+		//x(t+1)^T = x^T*G
+		//x(t+1)^T = apha * x(t)^T * P + (1-aplha)*pers^T
+		//OU
+		//(I - aplha*P)^T * x = (1-aplha) * v
 		return null;
 	}
 
