b47d0206432162aaca4f1073138982a8ea356038
CelineDknp <celine.deknop@student.uclouvain.be>
2016-03-30 23:30:25 +0200

    Full comments

--- main.java
+++ main.java
@@ -5,10 +5,10 @@ import java.util.*;
 public class main
 {
     public static void main(String[] args) {
-        System.out.println("Bienvenue dans ce programme de calcul PageRank!");
+        System.out.println("Bienvenue dans ce programme de calcul PageRank !");
         switch (args.length) {
             case 0:
-                System.out.println("Il n'y a pas de fichier à lire.");
+                System.out.println("Il n'y a pas de fichier à lire (Vous devez le spécifier en argument de la main).");
                 break;
             case 1:
                 System.out.println("Il y a un fichier à lire.");
@@ -26,21 +26,24 @@ public class main
         }
     }
 
+    /**
+     * Fonction qui lit un fichier contenant une matrice formatée et lance le calcul de pageRank sur celle-ci
+     */
     public static void openFile(String filename) throws IOException {
-        BufferedReader bf = new BufferedReader(new FileReader(filename));
+        BufferedReader bf = new BufferedReader(new FileReader(filename));//Initialisations des variables pour la lecture
         String line = bf.readLine();
-        String[] tmp = line.split(",");
+        String[] tmp = line.split(",");//On sépare la chaine sur ',' pour obtenir toutes les valeurs
         int l = tmp.length;
         double[][] mat = new double[l][l];
-        for (int y = 0; y < l; y++) {
+        for (int y = 0; y < l; y++) {//Boucle for pour la première ligne déjà lue à l'initialisation
             try {
-                mat[0][y] = Double.parseDouble(tmp[y]);
-            } catch (NumberFormatException e) {
+                mat[0][y] = Double.parseDouble(tmp[y]);//On essaye de lire un nombre et de l'insérer
+            } catch (NumberFormatException e) {//S'il est mal encodé, remplacer par un 0 et le signaler
                 System.err.println("Nombre à l'index (0," + y + ") mal encodé. Remplaçé par un 0.");
                 mat[0][y] = 0;
             }
         }
-        for (int x = 1; x < l; x++) {
+        for (int x = 1; x < l; x++) {//Boucle for pour le reste de la matrice
             line = bf.readLine();
             tmp = line.split(",");
             for (int y = 0; y < l; y++) {
@@ -53,13 +56,13 @@ public class main
             }
         }
         double[] q = new double[l];
-        for (int x = 0; x < l; x++) {
+        for (int x = 0; x < l; x++) {//On crée le vecteur de personnalisation, par défaut rempli de 1
             q[x] = 1;
         }
-        bf.close();
-        double[] ranked = pageRank(mat, 1, q);
-        print(ranked);
-        classement(ranked);
+        bf.close();//fermer le buffer
+        double[] ranked = pageRank(mat, 1, q);//Lancement du calcul de pageRank
+        print(ranked);//Affichage du résultat
+        classement(ranked);//Affichage du classement
     }
 
     public static double[] pageRank(double[][] adj, double alpha, double[] pers) {
@@ -94,33 +97,17 @@ public class main
 
     /**
      * Fonction qui vérifie si les deux matrices convergent, return true si elles sont égale, false sinon
+     * @ pre : a et b sont deux matrices ligne valides et de même tailles
+     * @ post : renvoie true si les valeurs de a et b convergent, false sinon
      */
     public static boolean converge(Matrix a, Matrix b){
-        double[] xA = a.getRowPackedCopy();
+        double[] xA = a.getRowPackedCopy();//Récupérer les deux matrices
         double[] xB = b.getRowPackedCopy();
         for(int i=0; i<xA.length; i++){
-            if(xB[i]-xA[i]>0.0001)
+            if(xB[i]-xA[i]>0.0001)//Si on trouve deux valeurs avec plus de 0.0001 d'écart, les valeurs ne convergent pas, renvoyer false
                 return false;
         }
-        return true;
-    }
-
-    /**
-     * @ pre : a est un vecteur valide
-     * @ post : renvoie la version normalisée de a, c'est-à-dire que la somme
-     *          des éléments du vecteur renvoyée est égale à 1
-     */
-    public static double[] normalize(double[] a) {
-        double count = 0;
-        for (int i = 0; i < a.length; i++)
-            count += a[i];
-        for (int i = 0; i < a.length; i++) {
-            if (count != 0)
-                a[i] = a[i] / count;
-            else
-                a[i] = 1.0 / a.length;
-        }
-        return a;
+        return true;//Si on est sorti de la boucle, les valeurs convergent
     }
 
     /**
@@ -152,23 +139,31 @@ public class main
         return a;
     }
 
+    /**
+     * Classe les valeurs du vecteur par ordre croissant
+     * @ pre : un tableau de double
+     * @ post : Affiche sur la sortie standard les indices du vecteur par ordre croissant
+     */
     public static void classement(double[] a) {
         System.out.print("\tClassement :");
         for (int i = 0; i < a.length; i++) {
-            int imax = maxIndice(a);
-            System.out.print(" " + (imax+1) + " ");
-            a[imax] = Double.MIN_VALUE;;
+            int imax = maxIndice(a);//Chercher l'indice de la valeur maximum
+            System.out.print(" " + (imax+1) + " ");//Afficher le résultat
+            a[imax] = Double.MIN_VALUE;//Stocker la valeur minimale dans la case pour ne plus qu'elle soit choisie
         }
         System.out.println();
     }
-
+    
+    /**
+     * Retourne l'indice de la valeur maximum contenue dans un tableau de double
+     */
     public static int maxIndice(double[] a) {
-        double max = Double.MIN_VALUE;
+        double max = Double.MIN_VALUE;//Initialisation
         int imax = 0;
         for (int i = 0; i < a.length; i++) {
-            if (a[i] > max) {
-                imax = i;
-                max = a[i];
+            if (a[i] > max) {//Si la valeur de cette case est meilleure que celle qu'on a actuellement
+                imax = i;//Stocker l'indice
+                max = a[i];//Stocker la valeur
             }
         }
         return imax;
